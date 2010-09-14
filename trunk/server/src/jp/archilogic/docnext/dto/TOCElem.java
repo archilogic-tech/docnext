9dc4a6b4ee7c64cf7589861a796850cf8b899ba5
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2010-09-14 07:40:18 +0000

    Implement annotation parser

--- trunk/server/src/jp/archilogic/docnext/dto/TOCElem.java
+++ trunk/server/src/jp/archilogic/docnext/dto/TOCElem.java
@@ -3,4 +3,12 @@ package jp.archilogic.docnext.dto;
 public class TOCElem {
     public int page;
     public String text;
+
+    public TOCElem() {
+    }
+
+    public TOCElem( int page , String text ) {
+        this.page = page;
+        this.text = text;
+    }
 }
