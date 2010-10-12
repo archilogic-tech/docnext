8bbb682fa68fa86f32ea9ab27c56be98794ae72b
k-ueno@fujisan.co.jp <k-ueno@fujisan.co.jp@85befead-b466-8c62-e381-11226e130421>
2010-10-12 01:25:52 +0000

    Add branche for server refactoring.

---
+++ branches/try-server-refactoring/server/src/jp/archilogic/docnext/logic/PackManager.java
@@ -0,0 +1,269 @@
+package jp.archilogic.docnext.logic;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import jp.archilogic.docnext.bean.PropBean;
+import jp.archilogic.docnext.dto.Region;
+import jp.archilogic.docnext.dto.TOCElem;
+import jp.archilogic.docnext.logic.PDFAnnotationParser.PageAnnotationInfo;
+import net.arnx.jsonic.JSON;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+@Component
+public class PackManager {
+    class Info {
+        public int pages;
+        public String title;
+        public String publisher;
+        public double ratio;
+    }
+
+    @SuppressWarnings( "unused" )
+    private static final Logger LOGGER = LoggerFactory.getLogger( PackManager.class );
+
+    @Autowired
+    private PropBean prop;
+
+    private void copyImageHelper( long documentId , String fileFormat ) {
+        String dir = prop.repository + "/pack/" + documentId + "/images/";
+
+        try {
+            for ( int index = 0 ; ; index++ ) {
+                File file = new File( String.format( fileFormat , prop.repository , documentId , index ) );
+                if ( !file.exists() ) {
+                    break;
+                }
+                FileUtils.copyFileToDirectory( file , new File( dir ) );
+            }
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void copyThumbnails( long documentId ) {
+        copyImageHelper( documentId , "%s/thumb/%d/thumb-%d.jpg" );
+    }
+
+    public void createStruct( long documentId ) {
+        String dir = prop.repository + "/pack/" + documentId + "/";
+
+        new File( dir ).mkdir();
+        new File( dir + "images/" ).mkdir();
+        new File( dir + "texts/" ).mkdir();
+
+        Info info = new Info();
+        info.title = "NO TITLE";
+        info.publisher = "NO PUBLISHER";
+        writeInfo( documentId , info );
+    }
+
+    private void encode( ZipOutputStream zos , File[] files , int prefixLen ) throws Exception {
+        for ( File f : files ) {
+            if ( f.isDirectory() ) {
+                encode( zos , f.listFiles() , prefixLen );
+            } else {
+                zos.putNextEntry( new ZipEntry( f.getPath().substring( prefixLen ).replace( '\\' , '/' ) ) );
+
+                InputStream is = new BufferedInputStream( new FileInputStream( f ) );
+                byte[] buf = new byte[ 1024 ];
+                for ( ; ; ) {
+                    int len = is.read( buf );
+                    if ( len < 0 ) {
+                        break;
+                    }
+                    zos.write( buf , 0 , len );
+                }
+                is.close();
+                zos.closeEntry();
+            }
+        }
+    }
+
+    public String getPackPath( long documentId ) {
+        return String.format( "%s/pack/%d.zip" , prop.repository , documentId );
+    }
+
+    private Info readInfo( long documentId ) {
+        try {
+            return JSON.decode( FileUtils.readFileToString( new File( String.format( "%s/pack/%d/info.json" ,
+                    prop.repository , documentId ) ) ) , Info.class );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public String readPublisher( long documentId ) {
+        return readInfo( documentId ).publisher;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public List< Integer > readSinglePageInfo( long documentId ) {
+        try {
+            return Arrays.asList( JSON.decode( FileUtils.readFileToString( new File( String.format(
+                    "%s/pack/%d/singlePageInfo.json" , prop.repository , documentId ) ) ) , Integer[].class ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public String readText( long documentId , int page ) {
+        try {
+            return FileUtils.readFileToString( new File( String.format( "%s/pack/%d/texts/%d" , prop.repository ,
+                    documentId , page ) ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public String readTitle( long documentId ) {
+        return readInfo( documentId ).title;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public List< TOCElem > readTOC( long documentId ) {
+        try {
+            return Arrays.asList( JSON.decode( FileUtils.readFileToString( new File( String.format(
+                    "%s/pack/%d/toc.json" , prop.repository , documentId ) ) ) , TOCElem[].class ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void repack( long documentId ) {
+        try {
+            String packed = prop.repository + "/pack/" + documentId + ".zip";
+            String dir = prop.repository + "/pack/" + documentId + "/";
+
+            ZipOutputStream zos = new ZipOutputStream( new FileOutputStream( packed ) );
+            encode( zos , new File[] { new File( dir ) } , dir.length() );
+            zos.close();
+        } catch ( FileNotFoundException e ) {
+            throw new RuntimeException( e );
+        } catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeAnnotations( long documentId , int page , List< PageAnnotationInfo > infos ) {
+        try {
+            FileUtils
+                    .writeStringToFile(
+                            new File( String.format( "%s/pack/%d/images/%d.anno.json" , prop.repository , documentId ,
+                                    page ) ) , JSON.encode( infos ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeImageText( long documentId , int page , String text ) {
+        try {
+            FileUtils.writeStringToFile(
+                    new File( String.format( "%s/pack/%d/texts/%d.image.txt" , prop.repository , documentId , page ) ) ,
+                    text );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    private void writeInfo( long documentId , Info info ) {
+        try {
+            FileUtils.writeStringToFile(
+                    new File( String.format( "%s/pack/%d/info.json" , prop.repository , documentId ) ) ,
+                    JSON.encode( info ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writePages( long documentId , int pages ) {
+        Info info = readInfo( documentId );
+        info.pages = pages;
+        writeInfo( documentId , info );
+    }
+
+    public void writePublisher( long documentId , String publisher ) {
+        Info info = readInfo( documentId );
+        info.publisher = publisher;
+        writeInfo( documentId , info );
+    }
+
+    public void writeRatio( long documentId , double ratio ) {
+        Info info = readInfo( documentId );
+        info.ratio = ratio;
+        writeInfo( documentId , info );
+    }
+
+    public void writeRegions( long documentId , int page , List< Region > regions ) {
+        final int SIZEOF_DOUBLE = 8;
+        final int N_REGION_FIELDS = 4;
+
+        try {
+            ByteBuffer buffer = ByteBuffer.allocate( regions.size() * N_REGION_FIELDS * SIZEOF_DOUBLE );
+            buffer.order( ByteOrder.LITTLE_ENDIAN );
+            for ( Region region : regions ) {
+                buffer.putDouble( region.x );
+                buffer.putDouble( region.y );
+                buffer.putDouble( region.width );
+                buffer.putDouble( region.height );
+            }
+            FileUtils.writeByteArrayToFile(
+                    new File( String.format( "%s/pack/%d/texts/%d.regions" , prop.repository , documentId , page ) ) ,
+                    buffer.array() );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeSinglePageInfo( long documentId , List< Integer > singlePageInfo ) {
+        try {
+            FileUtils.writeStringToFile(
+                    new File( String.format( "%s/pack/%d/singlePageInfo.json" , prop.repository , documentId ) ) ,
+                    JSON.encode( singlePageInfo ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeText( long documentId , int page , String text ) {
+        try {
+            FileUtils.writeStringToFile(
+                    new File( String.format( "%s/pack/%d/texts/%d" , prop.repository , documentId , page ) ) , text );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void writeTitle( long documentId , String title ) {
+        Info info = readInfo( documentId );
+        info.title = title;
+        writeInfo( documentId , info );
+    }
+
+    public void writeTOC( long documentId , List< TOCElem > toc ) {
+        try {
+            FileUtils.writeStringToFile(
+                    new File( String.format( "%s/pack/%d/toc.json" , prop.repository , documentId ) ) ,
+                    JSON.encode( toc ) );
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+}
---
+++ branches/try-server-refactoring/server/src/jp/archilogic/docnext/logic/UploadProcessor.java
@@ -0,0 +1,129 @@
+package jp.archilogic.docnext.logic;
+
+import java.io.File;
+import java.util.List;
+
+import jp.archilogic.docnext.bean.PropBean;
+import jp.archilogic.docnext.dao.DocumentDao;
+import jp.archilogic.docnext.dto.TOCElem;
+import jp.archilogic.docnext.entity.Document;
+import jp.archilogic.docnext.exception.UnsupportedFormatException;
+import jp.archilogic.docnext.logic.PDFAnnotationParser.PageAnnotationInfo;
+import jp.archilogic.docnext.logic.PDFTextParser.PageTextInfo;
+import jp.archilogic.docnext.util.FileUtil;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeException;
+import com.google.common.collect.Lists;
+import com.sun.star.task.ErrorCodeIOException;
+
+@Component
+public class UploadProcessor {
+    private class UploadTask implements Runnable {
+        private final String tempPath;
+        private final Document doc;
+
+        public UploadTask( String tempPath , Document doc ) {
+            this.tempPath = tempPath;
+            this.doc = doc;
+        }
+
+        @Override
+        public void run() {
+            try {
+                FileUtils.copyFile( new File( tempPath ) , new File( prop.repository + "/raw/" + doc.id ) );
+
+                String tempPdfPath = saveAsPdf( doc.fileName , tempPath , doc.id );
+                String ppmPath = FileUtil.createSameDirPath( tempPath , "ppm" );
+
+                packManager.createStruct( doc.id );
+
+                {
+                    int page = 0;
+                    for ( List< PageAnnotationInfo > infos : pdfAnnotationParser.parse( tempPdfPath ) ) {
+                        packManager.writeAnnotations( doc.id , page , infos );
+
+                        page++;
+                    }
+                }
+
+                String cleanedPath =
+                        FilenameUtils.getFullPathNoEndSeparator( tempPdfPath ) + File.separator + "cleaned" + doc.id
+                                + ".pdf";
+                pdfAnnotationParser.clean( tempPdfPath , cleanedPath );
+
+                double ratio =
+                        thumbnailCreator.create( prop.repository + "/thumb/" + doc.id + "/" , cleanedPath , ppmPath
+                                + doc.id );
+
+                packManager.copyThumbnails( doc.id );
+                packManager.writePages( doc.id , thumbnailCreator.getPages( cleanedPath ) );
+                packManager.writeTOC( doc.id , Lists.newArrayList( new TOCElem( 0 , "Chapter" ) ) );
+                packManager.writeSinglePageInfo( doc.id , Lists.< Integer > newArrayList() );
+                packManager.writeRatio( doc.id , ratio );
+
+                {
+                    int page = 0;
+                    for ( PageTextInfo pageTextInfo : pdfTextParser.parse( cleanedPath ) ) {
+                        packManager.writeText( doc.id , page , String.format( "<t>%s</t>" , pageTextInfo.text ) );
+                        packManager.writeImageText( doc.id , page , pageTextInfo.text );
+                        packManager.writeRegions( doc.id , page , pageTextInfo.regions );
+
+                        page++;
+                    }
+                }
+
+                packManager.repack( doc.id );
+
+                doc.processing = false;
+                documentDao.update( doc );
+            } catch ( Throwable e ) {
+                throw new RuntimeException( e );
+            }
+        }
+
+        private String saveAsPdf( String path , String tempPath , long documentId ) {
+            try {
+                if ( FilenameUtils.getExtension( path ).equals( "pdf" ) ) {
+                    return tempPath;
+                } else {
+                    String tempDir = FilenameUtils.getFullPathNoEndSeparator( tempPath );
+                    String tempPdfPath = tempDir + File.separator + "temp" + documentId + ".pdf";
+                    converter.convert( new File( tempPath ) , new File( tempPdfPath ) );
+                    return tempPdfPath;
+                }
+            } catch ( OpenOfficeException e ) {
+                // currently, unnecessary code though...
+                if ( e.getCause() instanceof ErrorCodeIOException ) {
+                    throw new UnsupportedFormatException();
+                } else {
+                    throw e;
+                }
+            }
+        }
+    }
+
+    @Autowired
+    private ThumbnailCreator thumbnailCreator;
+    @Autowired
+    private DocumentConverter converter;
+    @Autowired
+    private PropBean prop;
+    @Autowired
+    private DocumentDao documentDao;
+    @Autowired
+    private PackManager packManager;
+    @Autowired
+    private PDFTextParser pdfTextParser;
+    @Autowired
+    private PDFAnnotationParser pdfAnnotationParser;
+
+    public void proc( String tempPath , Document doc ) {
+        new UploadTask( tempPath , doc ).run();
+    }
+}
