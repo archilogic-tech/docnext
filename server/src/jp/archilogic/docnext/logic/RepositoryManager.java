0c03e62445596b2da933b094e7ea5a63fb9e549b
k-ueno@fujisan.co.jp <k-ueno@fujisan.co.jp@85befead-b466-8c62-e381-11226e130421>
2010-10-28 02:49:50 +0000

    Merge r83  Add creating pc client image.

--- server/src/jp/archilogic/docnext/logic/RepositoryManager.java
+++ server/src/jp/archilogic/docnext/logic/RepositoryManager.java
@@ -10,8 +10,12 @@ public class RepositoryManager {
     @Autowired
     private PropBean prop;
 
-    public String getImagePath( String type , long documentId , int page , int level , int px , int py ) {
-        return String.format( "%s/thumb/%d/%s%d-%d-%d-%d.jpg" , prop.repository , documentId , type , page , level ,
+    public String getImagePath( String type , long docId , int page ) {
+        return String.format( "%s/thumb/%d/%s-%d.jpg" , prop.repository , docId , type , page );
+    }
+
+    public String getImagePath( String type , long docId , int page , int level , int px , int py ) {
+        return String.format( "%s/thumb/%d/%s%d-%d-%d-%d.jpg" , prop.repository , docId , type , page , level ,
                 px , py );
     }
 }
