3f55c7c0dfc0eaa6041d7100e5e09eb010726090
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-02-02 07:16:14 +0000

    Init import

---
+++ trunk/android-client/src/jp/archilogic/docnext/android/task/BitmapReceiver.java
@@ -0,0 +1,7 @@
+package jp.archilogic.docnext.android.task;
+
+import android.graphics.Bitmap;
+
+public interface BitmapReceiver {
+    void receiver( Bitmap result );
+}
---
+++ trunk/android-client/src/jp/archilogic/docnext/android/task/IntegerReceiver.java
@@ -0,0 +1,5 @@
+package jp.archilogic.docnext.android.task;
+
+public interface IntegerReceiver {
+    void receiver( Integer result );
+}
