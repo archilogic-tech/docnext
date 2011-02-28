17484dee57bea9e7b83d3b667b7f275e4290fe3c
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-02-28 01:54:08 +0000

    Move old android client to branch

--- android-client/src/jp/archilogic/docnext/android/task/BitmapReceiver.java
+++
@@ -1,7 +0,0 @@
-package jp.archilogic.docnext.android.task;
-
-import android.graphics.Bitmap;
-
-public interface BitmapReceiver {
-    void receiver( Bitmap result );
-}
--- android-client/src/jp/archilogic/docnext/android/task/IntegerReceiver.java
+++
@@ -1,5 +0,0 @@
-package jp.archilogic.docnext.android.task;
-
-public interface IntegerReceiver {
-    void receiver( Integer result );
-}
