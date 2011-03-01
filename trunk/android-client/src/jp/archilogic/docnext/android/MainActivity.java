45134fc368faf9becd049de0db1622e58d318c08
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-03-01 10:25:38 +0000

    Implement communication with server Remove debug information

--- trunk/android-client/src/jp/archilogic/docnext/android/MainActivity.java
+++ trunk/android-client/src/jp/archilogic/docnext/android/MainActivity.java
@@ -1,55 +1,223 @@
 package jp.archilogic.docnext.android;
 
+import java.io.File;
+
+import jp.archilogic.docnext.android.info.MetaInfo;
 import jp.archilogic.docnext.android.manager.ConstManager;
+import jp.archilogic.docnext.android.task.DownloadTask;
+import jp.archilogic.docnext.android.task.Receiver;
+import jp.archilogic.docnext.android.type.ExtraType;
+import jp.archilogic.docnext.android.util.StorageUtil;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView _webView;
 
-    /*
-     * private void fetchFont() { final ProgressDialog progress = ProgressDialog.show( this , "" , "Loading..." );
-     * 
-     * new GetFontTask( this , new Receiver< Void , Void >() {
-     * 
-     * @Override public void error( final Void error ) { progress.dismiss(); }
-     * 
-     * @Override public void receive( final Void result ) { progress.dismiss();
-     * 
-     * startActivity( new Intent( MainActivity.this , TextViewerActivity.class ) ); } } , "default" ).execute(); }
-     * 
-     * private void fetchImagePage() { final ProgressDialog progress = new ProgressDialog( this ); progress.setMessage(
-     * "Loading..." ); progress.setCancelable( false ); progress.setProgressStyle( ProgressDialog.STYLE_HORIZONTAL );
-     * progress.setMax( N_PAGE * 2 );
-     * 
-     * progress.show();
-     * 
-     * fetchImagePage( 0 , progress ); }
-     * 
-     * private void fetchImagePage( final int page , final ProgressDialog progress ) { if ( page < N_PAGE ) {
-     * progress.setProgress( page );
-     * 
-     * new GetImagePageTask( this , new Receiver< Void , Void >() {
-     * 
-     * @Override public void error( final Void error ) { progress.dismiss(); }
-     * 
-     * @Override public void receive( final Void result ) { fetchImagePage( page + 1 , progress ); } } , DUMMY_ID , page
-     * ).execute(); } else { fetchImagePageThumbnail( 0 , progress ); } }
-     * 
-     * private void fetchImagePageThumbnail( final int page , final ProgressDialog progress ) { if ( page < N_PAGE ) {
-     * progress.setProgress( page + N_PAGE );
-     * 
-     * new GetImagePageThumbnailTask( this , new Receiver< Void , Void >() {
-     * 
-     * @Override public void error( final Void error ) { progress.dismiss(); }
-     * 
-     * @Override public void receive( final Void result ) { fetchImagePageThumbnail( page + 1 , progress ); } } ,
-     * DUMMY_ID , page ).execute(); } else { progress.dismiss();
-     * 
-     * startActivity( new Intent( this , ImageViewerActivity.class ) ); } }
-     */
+    private void checkMetaInfo( final long id ) {
+        final MetaInfo meta = StorageUtil.getMetaInfo( id );
+
+        switch ( meta.type ) {
+        case IMAGE:
+            fetchImage( id , meta );
+            break;
+        case TEXT:
+            fetchText( id , meta );
+            break;
+        default:
+            throw new RuntimeException();
+        }
+    }
+
+    private void fetchFont( final String name , final long id ) {
+        if ( new File( StorageUtil.getFontPath( name ) ).exists() ) {
+            showText( id );
+
+            return;
+        }
+
+        StorageUtil.ensureFontDir();
+
+        final ProgressDialog progress = ProgressDialog.show( this , "" , "Loading..." );
+
+        new DownloadTask( this , new Receiver< Void , Void >() {
+            @Override
+            public void error( final Void error ) {
+                progress.dismiss();
+            }
+
+            @Override
+            public void receive( final Void result ) {
+                progress.dismiss();
+
+                showText( id );
+            }
+        } , ConstManager.getInstance( MainActivity.this ).getEndpoint() + "font/" + name ,
+                StorageUtil.getFontPath( name ) ).execute();
+    }
+
+    private void fetchImage( final long id , final MetaInfo meta ) {
+        if ( new File( StorageUtil.getImagePath( id , meta.pages - 1 ) ).exists() ) {
+            fetchImageThumbnail( id , meta );
+
+            return;
+        }
+
+        StorageUtil.ensureImageDir( id );
+
+        final ProgressDialog progress = new ProgressDialog( this );
+        progress.setMessage( "Loading..." );
+        progress.setCancelable( false );
+        progress.setProgressStyle( ProgressDialog.STYLE_HORIZONTAL );
+        progress.setMax( meta.pages );
+
+        progress.show();
+
+        fetchImageHelper( id , meta , 0 , progress );
+    }
+
+    private void fetchImageHelper( final long id , final MetaInfo meta , final int page , final ProgressDialog progress ) {
+        if ( page < meta.pages ) {
+            progress.setProgress( page );
+
+            new DownloadTask( this , new Receiver< Void , Void >() {
+                @Override
+                public void error( final Void error ) {
+                    progress.dismiss();
+                }
+
+                @Override
+                public void receive( final Void result ) {
+                    fetchImageHelper( id , meta , page + 1 , progress );
+                }
+            } , ConstManager.getInstance( MainActivity.this ).getEndpoint() + "image/" + id + "/" + page ,
+                    StorageUtil.getImagePath( id , page ) ).execute();
+        } else {
+            progress.dismiss();
+
+            fetchImageThumbnail( id , meta );
+        }
+    }
+
+    private void fetchImageThumbnail( final long id , final MetaInfo meta ) {
+        if ( new File( StorageUtil.getImageThumbnailPath( id , meta.pages - 1 ) ).exists() ) {
+            showImage( id );
+
+            return;
+        }
+
+        StorageUtil.ensureImageDir( id );
+
+        final ProgressDialog progress = new ProgressDialog( this );
+        progress.setMessage( "Loading..." );
+        progress.setCancelable( false );
+        progress.setProgressStyle( ProgressDialog.STYLE_HORIZONTAL );
+        progress.setMax( meta.pages );
+
+        progress.show();
+
+        fetchImageThumbnailHelper( id , meta , 0 , progress );
+    }
+
+    private void fetchImageThumbnailHelper( final long id , final MetaInfo meta , final int page ,
+            final ProgressDialog progress ) {
+        if ( page < meta.pages ) {
+            progress.setProgress( page );
+
+            new DownloadTask( this , new Receiver< Void , Void >() {
+                @Override
+                public void error( final Void error ) {
+                    progress.dismiss();
+                }
+
+                @Override
+                public void receive( final Void result ) {
+                    fetchImageThumbnailHelper( id , meta , page + 1 , progress );
+                }
+            } , ConstManager.getInstance( MainActivity.this ).getEndpoint() + "imageThumbnail/" + id + "/" + page ,
+                    StorageUtil.getImageThumbnailPath( id , page ) ).execute();
+        } else {
+            progress.dismiss();
+
+            showImage( id );
+        }
+    }
+
+    private void fetchMetaInfo( final long id ) {
+        if ( new File( StorageUtil.getMetaInfoPath( id ) ).exists() ) {
+            checkMetaInfo( id );
+
+            return;
+        }
+
+        StorageUtil.ensureMetaInfoDir();
+
+        final ProgressDialog progress = ProgressDialog.show( this , "" , "Loading..." );
+
+        new DownloadTask( this , new Receiver< Void , Void >() {
+            @Override
+            public void error( final Void error ) {
+                progress.dismiss();
+            }
+
+            @Override
+            public void receive( final Void result ) {
+                progress.dismiss();
+
+                checkMetaInfo( id );
+            }
+        } , ConstManager.getInstance( MainActivity.this ).getEndpoint() + "meta/" + id + ".json" ,
+                StorageUtil.getMetaInfoPath( id ) ).execute();
+    }
+
+    private void fetchText( final long id , final MetaInfo meta ) {
+        if ( new File( StorageUtil.getTextPath( id , meta.pages - 1 ) ).exists() ) {
+            fetchFont( "default" , id );
+
+            return;
+        }
+
+        StorageUtil.ensureTextDir( id );
+
+        final ProgressDialog progress = new ProgressDialog( this );
+        progress.setMessage( "Loading..." );
+        progress.setCancelable( false );
+        progress.setProgressStyle( ProgressDialog.STYLE_HORIZONTAL );
+        progress.setMax( meta.pages );
+
+        progress.show();
+
+        fetchTextHelper( id , meta , 0 , progress );
+    }
+
+    private void fetchTextHelper( final long id , final MetaInfo meta , final int page , final ProgressDialog progress ) {
+        if ( page < meta.pages ) {
+            progress.setProgress( page );
+
+            new DownloadTask( this , new Receiver< Void , Void >() {
+                @Override
+                public void error( final Void error ) {
+                    progress.dismiss();
+                }
+
+                @Override
+                public void receive( final Void result ) {
+                    fetchTextHelper( id , meta , page + 1 , progress );
+                }
+            } , ConstManager.getInstance( MainActivity.this ).getEndpoint() + "text/" + id + "/" + page ,
+                    StorageUtil.getTextPath( id , page ) ).execute();
+        } else {
+            progress.dismiss();
+
+            fetchFont( "default" , id );
+        }
+    }
 
     @Override
     public void onCreate( final Bundle savedInstanceState ) {
@@ -58,37 +226,30 @@ public class MainActivity extends Activity {
 
         _webView = ( WebView ) findViewById( R.id.webView );
 
+        _webView.getSettings().setCacheMode( WebSettings.LOAD_NO_CACHE );
         _webView.setWebViewClient( new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading( final WebView view , final String url ) {
-                System.err.println( url );
+                final Uri uri = Uri.parse( url );
 
-                return true;
+                if ( uri.getScheme().equals( "docnext" ) ) {
+                    fetchMetaInfo( Long.parseLong( uri.getHost() ) );
+
+                    return true;
+                }
+
+                return false;
             }
         } );
 
         _webView.loadUrl( ConstManager.getInstance( this ).getEndpoint() + "library.html" );
+    }
 
-        /*
-         * findViewById( R.id.image ).setOnClickListener( new OnClickListener() {
-         * 
-         * @Override public void onClick( final View v ) { if ( new File( StorageUtil.getImageThumbnailPath( DUMMY_ID ,
-         * N_PAGE - 1 ) ).exists() ) { startActivity( new Intent( MainActivity.this , ImageViewerActivity.class ) );
-         * 
-         * return; }
-         * 
-         * StorageUtil.ensureImageDir( DUMMY_ID );
-         * 
-         * fetchImagePage(); } } ); findViewById( R.id.text ).setOnClickListener( new OnClickListener() {
-         * 
-         * @Override public void onClick( final View v ) { if ( new File( StorageUtil.getFontPath( "default" )
-         * ).exists() ) { startActivity( new Intent( MainActivity.this , TextViewerActivity.class ) );
-         * 
-         * return; }
-         * 
-         * StorageUtil.ensureFontDir();
-         * 
-         * fetchFont(); } } );
-         */
+    private void showImage( final long id ) {
+        startActivity( new Intent( this , ImageViewerActivity.class ).putExtra( ExtraType.ID.toString() , id ) );
+    }
+
+    private void showText( final long id ) {
+        startActivity( new Intent( this , TextViewerActivity.class ).putExtra( ExtraType.ID.toString() , id ) );
     }
 }
