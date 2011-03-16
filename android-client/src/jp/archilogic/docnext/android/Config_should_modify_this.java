655ecb0d629b86549b5e308ecbc068cb940dd9cf
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-03-16 05:10:55 +0000

    Refactor per app configuration

---
+++ android-client/src/jp/archilogic/docnext/android/Config_should_modify_this.java
@@ -0,0 +1,5 @@
+package jp.archilogic.docnext.android;
+
+public class Config_should_modify_this {
+    public static final String ENDPOINT = "http://docnext.example.com/";
+}
--- android-client/src/jp/archilogic/docnext/android/provider/remote/RemotePathManager.java
+++ android-client/src/jp/archilogic/docnext/android/provider/remote/RemotePathManager.java
@@ -1,25 +1,25 @@
 package jp.archilogic.docnext.android.provider.remote;
 
-import jp.archilogic.docnext.android.Kernel;
+import jp.archilogic.docnext.android.Config;
 
 public class RemotePathManager {
     public String getFontPath( final String name ) {
-        return Kernel.ENDPOINT + "font/" + name;
+        return Config.ENDPOINT + "font/" + name;
     }
 
     public String getImagePath( final long id , final int page , final int level , final int px , final int py ) {
-        return Kernel.ENDPOINT + "image/" + id + "/" + page + ".jpg";
+        return Config.ENDPOINT + "image/" + id + "/" + page + ".jpg";
     }
 
     public String getMetaInfoPath( final long id ) {
-        return Kernel.ENDPOINT + "meta/" + id + ".json";
+        return Config.ENDPOINT + "meta/" + id + ".json";
     }
 
     public String getTextInfoPath( final long id , final int page ) {
-        return Kernel.ENDPOINT + "text/" + id + "/" + page + ".json";
+        return Config.ENDPOINT + "text/" + id + "/" + page + ".json";
     }
 
     public String getThumbnailPath( final long id , final int page ) {
-        return Kernel.ENDPOINT + "thumb/" + id + "/" + page + ".jpg";
+        return Config.ENDPOINT + "thumb/" + id + "/" + page + ".jpg";
     }
 }
