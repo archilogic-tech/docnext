a685aec3b2208df799920c6056c894e9448bc0cf
tokutake@archilogic.jp <tokutake@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-03-23 05:14:16 +0000

    Several fixes.

--- trunk/server/war/js/edit_info.js
+++ trunk/server/war/js/edit_info.js
@@ -1,55 +1,43 @@
-load();
-function load() {
-  var id = readCookie('document_id');
+var id;
+
+$(document).ready(function(){
+  id = readCookie("document_id");
    
-  DocumentService.getTitle(id,
-    function(data) {
-      document.getElementById('title').value = data;
-    }
-  );
-  DocumentService.getPublisher(id,
-    function(data) {
-      document.getElementById('publisher').value = data;
-    }
-  );
-  DocumentService.getBinding(id,
-    function(data) {
-      if (data == 'left')
-        $('#binding_left')[0].checked = true;
-      else if (data == 'right')
-        $('#binding_right')[0].checked = true;
-    }
-  );
-  DocumentService.getFlow(id,
-    function(data) {
-      if (data == 'left')
-        $('#flow_left')[0].checked = true;
-      else if (data == 'right')
-        $('#flow_right')[0].checked = true;
-    }
-  );
-}
+  DocumentService.getTitle(id, function(data) {
+      $("#title").val(data);
+    });
+  DocumentService.getPublisher(id, function(data) {
+      $("#publisher").val(data);
+    });
+  DocumentService.getBinding(id, function(data) {
+      $("#binding_left").attr("checked", data == "left");
+      $("#binding_right").attr("checked", data == "right");
+    });
+  DocumentService.getFlow(id, function(data) {
+      $("#flow_left").attr("checked", data == "left");
+      $("#flow_right").attr("checked", data == "right");
+    });
+  
+  $("input").change(save);
+  //$("input").keydown(save);
+});
   
 function save() {
-  var id = readCookie('document_id');
-
-  var title = document.getElementById('title').value;
-  DocumentService.setTitle(id, title, function(data) {});
-
-  var publisher = document.getElementById('publisher').value;
-  DocumentService.setPublisher(id, publisher, function(data) {});
+  DocumentService.setTitle(id, $("#title").val(), function(data) {});
+  //DocumentService.setTitle(id, "養女", function(data) {});
+  DocumentService.setPublisher(id, $("#publisher").val(), function(data) {});
   
   var binding = "none";
-  if ($("#binding_left")[0].checked)
+  if ($("#binding_left").attr("checked"))
     binding = "left";
-  if ($("#binding_right")[0].checked)
+  if ($("#binding_right").attr("checked"))
     binding = "right";
   DocumentService.setBinding(id, binding, function() {});
 
   var flow = "none";
-  if ($("#flow_left")[0].checked)
+  if ($("#flow_left").attr("checked"))
     flow = "left";
-  if ($("#flow_right")[0].checked)
+  if ($("#flow_right").attr("checked"))
     flow = "right";
   DocumentService.setFlow(id, flow, function() {});
 }
