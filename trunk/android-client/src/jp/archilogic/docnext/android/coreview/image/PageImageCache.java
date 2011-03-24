48b758f863a4ea1b5f0c163b01d591f06cc4aaa4
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-03-24 03:14:26 +0000

    Implement multi-level zoom & loading

--- trunk/android-client/src/jp/archilogic/docnext/android/coreview/image/PageImageCache.java
+++ trunk/android-client/src/jp/archilogic/docnext/android/coreview/image/PageImageCache.java
@@ -4,12 +4,14 @@ import android.graphics.Bitmap;
 
 class PageImageCache {
     int page;
+    int level;
     int px;
     int py;
     Bitmap bitmap;
 
-    PageImageCache( final int page , final int px , final int py , final Bitmap bitmap ) {
+    PageImageCache( final int page , final int level , final int px , final int py , final Bitmap bitmap ) {
         this.page = page;
+        this.level = level;
         this.px = px;
         this.py = py;
         this.bitmap = bitmap;
