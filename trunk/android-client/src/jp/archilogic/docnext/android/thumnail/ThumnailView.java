a66af3042e42c4578b9ab13959bb1d996d316a5d
tokutake@archilogic.jp <tokutake@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-04-07 08:18:35 +0000

    Compose thumbnails from textures.

--- trunk/android-client/src/jp/archilogic/docnext/android/thumnail/ThumnailView.java
+++ trunk/android-client/src/jp/archilogic/docnext/android/thumnail/ThumnailView.java
@@ -1,6 +1,8 @@
 package jp.archilogic.docnext.android.thumnail;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -10,7 +12,10 @@ import jp.archilogic.docnext.android.activity.CoreViewActivity;
 import jp.archilogic.docnext.android.coreview.CoreView;
 import jp.archilogic.docnext.android.coreview.CoreViewDelegate;
 import jp.archilogic.docnext.android.info.DocInfo;
+import jp.archilogic.docnext.android.info.ImageInfo;
 import jp.archilogic.docnext.android.meta.DocumentType;
+import jp.archilogic.docnext.android.provider.local.LocalPathManager;
+import jp.archilogic.docnext.android.provider.local.LocalProvider;
 
 import org.apache.commons.io.IOUtils;
 
@@ -18,7 +23,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.PointF;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,35 +38,85 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 public class ThumnailView extends FrameLayout implements CoreView {
-    public class ThumnailImageAdapter extends BaseAdapter {
-        public long id = 0;
-        protected int count = 0;
+    private long id = 0;
 
-        public ThumnailImageAdapter( final long aId ) {
+    public class ThumnailImageAdapter extends BaseAdapter {
+        private int count = 0;
+        private LocalProvider provider = Kernel.getLocalProvider();
+        private LocalPathManager pathManager = new LocalPathManager();
+        private final String TAG = "Thumnail";
+
+        public ThumnailImageAdapter(final long aId) {
             id = aId;
 
-            final DocInfo doc = Kernel.getLocalProvider().getDocInfo( id );
+            final DocInfo doc = provider.getDocInfo( id );
             count = doc.pages;
         }
 
         private Bitmap getBitmap( final int page ) {
+            String thumnailPath = pathManager.getThumnailPath( id , page );
+            File thumnailFile = new File( thumnailPath );
+            if ( thumnailFile.exists() ) {
+                Bitmap thumnailBitmap = BitmapFactory.decodeFile( thumnailPath );
+                return thumnailBitmap;
+            }
+
+            ImageInfo imageInfo = provider.getImageInfo( id );
+            int height = imageInfo.height;
+            int width = imageInfo.width;
+            Bitmap bigBitmap = Bitmap.createBitmap( width , height , Bitmap.Config.RGB_565 );
+            Canvas canvas = new Canvas( bigBitmap );
+
+            // unsure whether getImagePath is correct method for thumnail.
             InputStream in = null;
-            Bitmap bitmap;
             try {
-                final int level = 0;
-                final int px = 0;
-                final int py = 0;
-
-                // unsure whether getImagePath is correct method for thumnail.
-                in = new FileInputStream( Kernel.getLocalProvider().getImagePath( id , page , level , px , py ) );
-
-                bitmap = BitmapFactory.decodeStream( in );
+                for ( int x = 0; x < Math.ceil( width / 512. ); x++ ) {
+                    for ( int y = 0; y < Math.ceil( height / 512. ); y++ ) {
+                        final int level = 0;
+                        Log.d( TAG , String.format( "id:%d, page:%d x:%d y:%d" , id , page , x , y ) );
+                        String path = provider.getImagePath( id , page , level , x , y );
+                        Log.d( "thumnail" , "path: " + path );
+                        if ( path == null ) {
+                            break;
+                        }
+                        in = new FileInputStream( path );
+                        Bitmap bitmap = BitmapFactory.decodeStream( in );
+                        canvas.drawBitmap( bitmap , 512 * x , 512 * y , null );
+                    }
+                }
             } catch ( final IOException e ) {
                 throw new RuntimeException( e );
             } finally {
                 IOUtils.closeQuietly( in );
             }
-            return bitmap;
+
+            Bitmap resizedBitmap = getResizedBitmap( bigBitmap , 176 , 256 );
+            try {
+                File file = new File( thumnailPath );
+                FileOutputStream out = new FileOutputStream( file );
+                resizedBitmap.compress( Bitmap.CompressFormat.JPEG , 90 , out );
+            } catch ( Exception exception ) {
+                Log.e( TAG , exception.getMessage() );
+            }
+            return resizedBitmap;
+        }
+
+        public Bitmap getResizedBitmap( Bitmap bitmap , int newWidth , int newHeight ) {
+
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            float scaleWidth = ( (float) newWidth ) / width;
+            float scaleHeight = ( (float) newHeight ) / height;
+
+            Matrix matrix = new Matrix();
+
+            matrix.postScale( scaleWidth , scaleHeight );
+
+            Bitmap resizedBitmap = Bitmap.createBitmap( bitmap , 0 , 0 , width , height , matrix ,
+                    false );
+
+            return resizedBitmap;
         }
 
         @Override
@@ -82,11 +140,11 @@ public class ThumnailView extends FrameLayout implements CoreView {
 
             if ( convertView == null ) {
                 imageView = new ImageView( getContext() );
-                imageView.setLayoutParams( new GridView.LayoutParams( 180 , 180 ) );
-                imageView.setScaleType( ImageView.ScaleType.CENTER_CROP );
-                imageView.setPadding( 8 , 8 , 8 , 8 );
+                // imageView.setLayoutParams( new GridView.LayoutParams( dp( 80
+                // ) , dp( 90 ) ) );
+                imageView.setScaleType( ImageView.ScaleType.FIT_CENTER );
             } else {
-                imageView = ( ImageView ) convertView;
+                imageView = (ImageView) convertView;
             }
 
             imageView.setImageBitmap( getBitmap( position ) );
@@ -98,78 +156,49 @@ public class ThumnailView extends FrameLayout implements CoreView {
         }
     }
 
-    private final GridView gridView;
-
+    private final GridView gridView = (GridView) findViewById( R.id.thumnail );
     private CoreViewDelegate delegate;
 
-    public ThumnailView( final Context context ) {
+    public ThumnailView(final Context context) {
         super( context );
 
         LayoutInflater.from( context ).inflate( R.layout.thumnail , this , true );
-
-        gridView = ( GridView ) findViewById( R.id.thumnail );
-        gridView.setOnItemClickListener( new OnItemClickListener() {
-            @Override
-            public void onItemClick( final AdapterView< ? > parent , final View v , final int position , final long id ) {
-                final Intent intent = new Intent();
-                intent.putExtra( CoreViewActivity.EXTRA_PAGE , position );
-                delegate.changeCoreViewType( DocumentType.IMAGE , intent );
-            }
-        } );
     }
 
     @Override
     public void onDoubleTapGesture( final PointF point ) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onDragGesture( final PointF delta ) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onFlingGesture( final PointF velocity ) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onGestureBegin() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onGestureEnd() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onPause() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onResume() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onTapGesture( final PointF point ) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onZoomGesture( final float scaleDelta , final PointF center ) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -177,10 +206,23 @@ public class ThumnailView extends FrameLayout implements CoreView {
         delegate = argumentDelegate;
     }
 
+    private void setGridView() {
+        final GridView gridview = (GridView) findViewById( R.id.thumnail );
+        gridview.setAdapter( new ThumnailImageAdapter( id ) );
+
+        gridView.setOnItemClickListener( new OnItemClickListener() {
+            @Override
+            public void onItemClick( final AdapterView< ? > parent , final View v ,
+                    final int position , final long id ) {
+                final Intent intent = new Intent();
+                intent.putExtra( CoreViewActivity.EXTRA_PAGE , position );
+                delegate.changeCoreViewType( DocumentType.IMAGE , intent );
+            }
+        } );
+    }
+    
     @Override
     public void setIds( final long[] ids ) {
-        final GridView gridview = ( GridView ) findViewById( R.id.thumnail );
-        final ThumnailImageAdapter adapter = new ThumnailImageAdapter( ids[ 0 ] );
-        gridview.setAdapter( adapter );
+        setGridView();
     }
 }
