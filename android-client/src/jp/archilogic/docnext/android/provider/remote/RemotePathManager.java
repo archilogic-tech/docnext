ea4d171ae9af86d557a42cd0764ab6e1eee83113
toyama@archilogic.jp <toyama@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-04-19 02:37:49 +0000

    Change endpoint URL format (param -> path)

--- android-client/src/jp/archilogic/docnext/android/provider/remote/RemotePathManager.java
+++ android-client/src/jp/archilogic/docnext/android/provider/remote/RemotePathManager.java
@@ -4,32 +4,32 @@ import jp.archilogic.docnext.android.Config;
 
 public class RemotePathManager {
     public String getDocInfoPath( final long id ) {
-        return Config.ENDPOINT + "viewer/getDocInfo?id=" + id;
+        return String.format( "%sviewer/getDocInfo/%d" , Config.ENDPOINT , id );
     }
 
     public String getFontPath( final String name ) {
-        return Config.ENDPOINT + "viewer/getFont?name=" + name;
+        return String.format( "%sviewer/getFont/%s" , Config.ENDPOINT , name );
     }
 
     public String getImageInfoPath( final long id , final int shortSide ) {
-        return Config.ENDPOINT + "viewer/getImageInfo?id=" + id + "&shortSide=" + shortSide;
+        return String.format( "%sviewer/getImageInfo/%d/%d" , Config.ENDPOINT , id , shortSide );
     }
 
-    public String getImagePath( final long id , final int page , final int level , final int px , final int py ,
-            final int shortSide ) {
-        return String.format( "%sviewer/smartGetPage?id=%d&page=%d&level=%d&px=%d&py=%d&shortSide=%d" ,
-                Config.ENDPOINT , id , page , level , px , py , shortSide );
+    public String getImagePath( final long id , final int page , final int level , final int px ,
+            final int py , final int shortSide ) {
+        return String.format( "%sviewer/smartGetPage/%d/%d/%d/%d/%d/%d" , Config.ENDPOINT , id ,
+                page , level , px , py , shortSide );
     }
 
     public String getTableOfContentsPath( final long id ) {
-        return Config.ENDPOINT + "viewer/getTOC?id=" + id;
+        return String.format( "%sviewer/getTOC/%d" , Config.ENDPOINT , id );
     }
 
     public String getTextPath( final long id , final int page ) {
-        return String.format( "%sviewer/getText?id=%d&page=%d" , Config.ENDPOINT , id , page );
+        return String.format( "%sviewer/getText/%d/%d" , Config.ENDPOINT , id , page );
     }
-    
+
     public String getThumbnailPath( final long id , final int page ) {
-        return Config.ENDPOINT + String.format( "viewer/getThumbnail?id=%d&page=%d" , id , page ); 
+        return String.format( "%sviewer/getThumbnail/%d/%d" , Config.ENDPOINT , id , page );
     }
 }
