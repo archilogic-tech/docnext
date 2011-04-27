3f917da0ff77a0cfdbad01ab3573340a4d8342ff
tokutake@archilogic.jp <tokutake@archilogic.jp@85befead-b466-8c62-e381-11226e130421>
2011-04-27 09:13:59 +0000

    Interpret font size as sp ( scalable font , scale size with user preference and display density

--- trunk/android-client/src/jp/archilogic/docnext/android/coreview/text/CoreTextConfig.java
+++ trunk/android-client/src/jp/archilogic/docnext/android/coreview/text/CoreTextConfig.java
@@ -4,6 +4,7 @@ import jp.archilogic.docnext.android.setting.Setting;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.util.TypedValue;
 
 public class CoreTextConfig {
     public static enum LineBreakingRule {
@@ -12,7 +13,7 @@ public class CoreTextConfig {
 
     // typesettings
     public TextDocDirection direction;
-    public int fontSize;
+    public int fontSize; // unit is piont( not sp )
     public float lineSpace;
     public float horizontalPadding;
     public float verticalPadding;
@@ -26,8 +27,12 @@ public class CoreTextConfig {
 
     // not popular config
     public int rubyFontSizeFactor;
+    
+    public Context _context;
 
     CoreTextConfig( Context context ) {
+        _context = context;
+        
         direction = TextDocDirection.HORIZONTAL;
         fontSize = 16;
         lineSpace = 4;
@@ -44,6 +49,12 @@ public class CoreTextConfig {
         
         SharedPreferences prefs = context.getSharedPreferences( "viewer" , Context.MODE_PRIVATE );
         fontSize = prefs.getInt( Setting.PREF_FONT_SIZE , Setting.DEFAULT_FONT_SIZE );
+        
+        int sp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 
+                (float) fontSize , _context.getResources().getDisplayMetrics());
+
+        fontSize = sp;
+        
         backgroundColor = prefs.getInt( Setting.PREF_BACKGROUND_COLOR , Setting.DEFAULT_BACKGROUND_COLOR );
         defaultTextColor = prefs.getInt( Setting.PREF_FONT_COLOR , Setting.DEFAULT_FONT_COLOR );
         String writingMode = prefs.getString( Setting.PREF_WRITING_MODE , Setting.DEFAULT_WRITING_MODE );
